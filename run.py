685518cac880cc5faac3a8f88e7861c6a037c7bc
MythicalMayhem <ameuroussema@gmail.com>
2022-05-03 16:31:29 +0000

    Create run.py

---
+++ run.py
@@ -0,0 +1,6 @@
+for l in range(1,256):
+    for m in range(1,256):
+        for n in range(1,256):
+            for o in range(1,256):
+                p = str(l)+'.'+str(m)+'.'+str(n)+'.'+str(o)
+                print(p)
